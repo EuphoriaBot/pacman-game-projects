4974cde2e043d9c551bf4912cecd10618e0742b1
EuphoriaBot <dimasfirja80@gmail.com>
2026-07-05 18:33:42 +0800

    Ghost Range

--- ghost.js
+++ ghost.js
@@ -18,7 +18,7 @@ class Ghost {
     }
 
     changeRandomDirection() {
-        this.randomTargetIndex += 1;
+        this.randomTargetIndex += parseInt(Math, random() * 4);
         this.randomTargetIndex = this.randomTargetIndex % 4;
     }
 
@@ -195,6 +195,12 @@ class Ghost {
             ghostFrames, this.imageX, this.imageY, this.imageWidth, this.imageHeight, this.x, this.y, this.width, this.height
         );
         canvasContext.restore();
+        canvasContext.beginPath();
+        canvasContext.strokeStyle = "red"
+        canvasContext.arc(
+            this.x + oneBlockSize / 2, this.y + oneBlockSize / 2, this, range * oneBlockSize, 0, 2 * Math.PI
+        );
+        canvasContext.stroke()
     }
 
     getMapX() {
